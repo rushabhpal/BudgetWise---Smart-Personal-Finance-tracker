9dc62af5b6554060509548685cf99d7721778204
DeveloperRP <rushabhinvest143@gmail.com>
2025-08-08 03:57:37 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,52 +1,46 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const connectDB = require('./config/db');
-
-const authRoutes = require('./routes/authRoutes');
-const incomeRoutes = require('./routes/incomeRoutes');
-const expenseRoutes = require('./routes/expenseRoutes');
-const dashboardRoutes = require('./routes/dashboardRoutes');
-
+const path = require('path');
+const connectDB = require('./config/db')
+const authRoutes = require('./routes/authRoutes')
+const incomeRoutes = require('./routes/incomeRoutes')
+const expenseRoutes = require('./routes/expenseRoutes')
+const dashboardRoutes = require('./routes/dashboardRoutes')
 const app = express();
-
+ 
 const allowedOrigins = [
   'http://localhost:5173',
-  'https://budgetwise-rushabhpal.netlify.app'
+  '"https://budgetwise-rushabhpal.netlify.app"'
 ];
-
 app.use((req, res, next) => {
-  console.log('Incoming Origin:', req.headers.origin);
+  console.log('Origin:', req.headers.origin);
   next();
 });
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
+  })
+);
 
-app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true
-}));
 
-app.options('*', cors());
-
-// Middleware
 app.use(express.json());
-
-// Connect DB
 connectDB();
 
-// API Routes
-app.use('/api/v1/auth', authRoutes);
-app.use('/api/v1/income', incomeRoutes);
-app.use('/api/v1/expense', expenseRoutes);
-app.use('/api/v1/dashboard', dashboardRoutes);
+app.use('/api/v1/auth',authRoutes);
+app.use('/api/v1/income',incomeRoutes);
+app.use('/api/v1/expense',expenseRoutes);
+app.use('/api/v1/dashboard',dashboardRoutes);
 
-// Start server
+
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT,()=>console.log(`Server runing on port ${PORT}`))
